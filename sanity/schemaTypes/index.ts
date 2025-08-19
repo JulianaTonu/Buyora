287fb22b1618bd5a6a738985249a616f6be39b34
julianatonu <julianatonu@gmail.com>
2025-08-19 15:38:57 +0600

    schemaType

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,17 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './categoryType'
+import { addressType } from './addressType'
+import { blockContentType } from './blockContentType'
+import { blogType } from './blogType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType],
+  types: [categoryType,
+    // authorType,
+    blockContentType,
+    blogType,
+    //  brandType,
+    //  orderType,
+    //  productType,
+     addressType
+    ],
 }
