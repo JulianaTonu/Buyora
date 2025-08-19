7a01c0493dfde669a853965c790c3596d925389f
julianatonu <julianatonu@gmail.com>
2025-08-20 00:03:38 +0600

    ProductGrid

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -2,15 +2,22 @@ import { productType } from '@/constants/data';
 import Link from 'next/link';
 import React from 'react';
 
-const HomeTabBar = () => {
+interface Props {
+    selectedTab:string;
+    onTabSelect:(tab:string)=>void
+}
+const HomeTabBar = ({selectedTab, onTabSelect}) => {
     return (
         <div className='flex items-center justify-between'>
             <div className='flex items-center gap-3 font-semibold text-sm'>
                 {productType?.map((item)=>(
                     <button
+                    onClick={()=>onTabSelect(item?.title)}
                      key={item.title}
-                     className={`border border-dark_purple/30 px-4 py-1.5 hover:bg-lightColor hover:text-white md:px-6 md:py-2 rounded-full  hoverEffect `}
-                     >{item?.title}</button>
+                     className={`border border-dark_purple/30 px-4 py-1.5 hover:bg-lightColor hover:text-white md:px-6 md:py-2 rounded-full  hoverEffect ${selectedTab === item?.title? "bg-lightColor text-white border-lightColor" : "bg-lightColor/20"}`}
+                     >
+                        {item?.title}
+                     </button>
                 ))}
             </div>
             <Link href={"/shop"}
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,12 +1,44 @@
 "use client"
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HomeTabBar from './HomeTabBar';
+import { productType } from '@/constants/data';
+import { client } from '@/sanity/lib/client';
 
 const ProductGrid = () => {
+    const [products, setProducts] =useState([])
+    const [loading, setLoading] =useState(false)
+    const [selectedTab, setSelectedTab] =useState(productType[0]?.title || "")
+
+const query =`*[_type == "product" && variant==$variant]| order(name desc) {
+  ...,"categories":categories[]->title
+ 
+}`;
+
+const params ={variant:selectedTab.toLowerCase()}
+
+useEffect(()=>{
+const fetchData =async ()=>{
+setLoading(true);
+try {
+    const response =await client.fetch(query,params)
+    console.log(response)
+} catch (error) {
+    console.error("Product fetching error",error)
+}finally{
+    setLoading(false);
+}}
+fetchData()
+},[selectedTab])
     return (
         <div>
-            <HomeTabBar/>
+            <HomeTabBar
+             selectedTab={selectedTab} 
+             onTabSelect={setSelectedTab}
+             />
+             {loading? <div> 
+
+             </div> : <>products</>}
         </div>
     );
 };
