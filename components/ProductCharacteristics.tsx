405a2f1f43ebbd9d8fd2714423fc642f25ae0ef0
julianatonu <julianatonu@gmail.com>
2025-09-23 17:06:58 +0600

    add

--- components/ProductCharacteristics.tsx
+++ components/ProductCharacteristics.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Accordion, AccordionItem, AccordionTrigger } from './ui/accordion';
 import { AccordionContent } from '@radix-ui/react-accordion';
 
+
 const ProductCharacteristics = async ({
     product,
 
@@ -15,11 +16,36 @@ const ProductCharacteristics = async ({
             <Accordion type='single' collapsible>
                 <AccordionItem value='item-1'>
                     <AccordionTrigger>
-                        {product?.name}:Characteristics
+                        {product?.name}:Characteristics </AccordionTrigger>
                         <AccordionContent>
-                            <p>Brand:{brand && <span>{brand[0]?.brandName}</span>}</p>
+                            <p className='flex items-center justify-between'>
+                                Brand:{""}
+                                {brand && (
+                                    
+                                    <span className='font-semibold tracking-wide'>
+                                        {brand[0]?.brandName}
+                                        </span>
+                                        )}
+                                        </p>
+                            <p className='flex items-center justify-between'>
+                                Collection{""}
+                                <span className='font-semibold tracking-wide'>2025</span>
+                            </p>
+                           
+                            <p className='flex items-center justify-between'>
+                                Type{""}
+                                <span className='font-semibold tracking-wide'>
+                                    {product?.variant}
+                                </span>
+                            </p>
+                            <p className='flex items-center justify-between'>
+                                Stock{""}
+                                <span className='font-semibold tracking-wide'>
+                                 {product?.stock ? "Available" : "Out of Stock"}
+                                </span>
+                            </p>
                         </AccordionContent>
-                    </AccordionTrigger>
+                    
                 </AccordionItem>
             </Accordion>
             ProductCharacteristics
