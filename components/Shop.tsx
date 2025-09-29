52abd8265566c92eef64a5cc776397edf6e1b9fa
julianatonu <julianatonu@gmail.com>
2025-09-29 12:13:58 +0600

    shop

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -12,8 +12,17 @@ const Shop = ({categories, brand}: Props) => {
     return (
         <div className='border-t'>
             <Container className='mt-5'>
-                <div>
+                <div className='sticky top-0 z-10 mb-5'>
+                    <div className='flex items-center justify-between'>
+                        <p className='text-lg uppercase tracking-wide'>Get the product as you need </p>
+                        <button className='text-black underline text-sm mt-2 font-medium hover:text-darkColor hoverEffect'>Reset Filters</button>
+                    </div>
                 </div>
+                <div>
+                    <div>Filter value</div>
+                    <div>Products</div>
+                </div>
+                
             </Container>
         </div>
     );
